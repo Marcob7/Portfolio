a69adb43d51ba9ee1d3e7e859609a1a66453d7b8
Marcob7 <marcobuitenhuis402@hotmail.com>
2022-10-21 22:06:09 +0200

    removed goo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 //import {HiArrowNarrowRight} from 'react-icons/hi'
 import ProfilePic from '../Assets/profilephoto.jpg';
-import Goo from 'gooey-react';
+//import Goo from 'gooey-react';
 
 const Home = () => {
 
   return (
-    <>
+    <> 
     
 
 
@@ -16,7 +16,7 @@ const Home = () => {
           
             <div>
             </div>
-        </div>
+        </div> 
         
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-4 px-4'>
             <div className='sm:text-left text-4xl font-bold'>
@@ -47,18 +47,7 @@ const Home = () => {
                   <img src={ProfilePic} alt="profielfoto" className="border:radius-[5px] shadow-lg rounded max-w-full h-auto align-middle border-none" />
 
                 </div> 
-                <Goo className="mt-8" intensity="strong" id="1">
-<svg width="192" height="192">
-<g style={{ animation: 'left 4s linear infinite' }}>
-<circle style={{ animation: 'left 3s ease-in-out infinite' }} cx="37%" cy="47%" fill="#003082" r="45" />
-
-<circle style={{ animation: 'right 1s linear infinite' }} cx="63%" cy="53%" fill="#0a5ae6" r="32" />
-<circle style={{ animation: 'right 3s linear infinite' }} cx="63%" cy="53%" fill="#0a5ae6" r="32" />
-<circle style={{ animation: 'left 2s linear infinite' }} cx="37%" cy="47%" fill="#003082" r="45" />
-</g>
-</svg>
-
-</Goo>
+    
 
                 </div>
                 </div>   
